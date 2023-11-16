c2b1b7d45fb5347bf11ec0f59dae7e590b21674d
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-11-16 14:03:49 +0530

    Update lcs.cpp

--- Divide_and_Conquer/lcs.cpp
+++ Divide_and_Conquer/lcs.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
 string s1 = "PQRSTPQRS";
 string s2 = "PRATPBRQRPS";
+int m = s1.size();
+int n = s2.size();
 
-int max(int a,int b){
-    return (a>b)? a:b;
+int max(int n,int m){
+    return (n>m)? n:m;
 }
 
 int lcs(int n,int m){
-    if(n == 0 || m == 0) return 0;
-    else if(s1[n-1] == s2[m-1]) return 1 +lcs(n-1,m-1);
-    else return max(lcs(n,m-1),lcs(n-1,m));
+    int dp[n+1][m+1];
+    for(int i=0;i<=n;++i) dp[i][0]=0;
+    for(int j=0;j<=m;++j) dp[0][j]=0;
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=m;++j){
+            if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+            else dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+        }
+    }
+    return dp[n][m];
 }
 
 int main(){
-    int n = s1.size();
-    int m = s2.size();
-    cout << lcs(n,m) << endl;
+    cout << lcs(m,n) << endl;
     return 0;
 }
